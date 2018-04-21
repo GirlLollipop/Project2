38d9ee691bd978ba880f11bef95d988579c50bd3
DianaColm <dcolmenerog@yahoo.com.mx>
2018-04-20 21:28:21 -0500

    primeros componentes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'; //Este import va en todas las vistas, hace referencia a un pedacito de react
+import './App.css'; //el .css sirve para dar estilos
+import { Route, Switch } from "react-router-dom"; //hay que importar Route y switch ya que los ocuparemos
+import Home from'./components/Home.js' // importo componente de Home
+import LogIn from './components/LogIn.js' // importo componente de LogIn
+import Splash from 'react-splash';
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+      <Splash
+  text="Project Name"
+  src="https://source.unsplash.com/FxU8KV7psMY/1600x900"
+  style={{ height: '70vh' }}
+/>
+
+      <Switch> //entre los switch escribimos los componentes que van a hacer un "cambio de vista"
+        <Route component={LogIn} path="/" exact />
+        <Route component={Home} path="/Home" />
+      </Switch>
       </div>
     );
   }
---
+++ src/components/Home.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+
+        <div id="accordion">
+          <div className="card">
+            <div className="card-header" id="headingOne">
+              <h5 className="mb-0">
+              <button className="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+              Cuenta perfiles
+              <br/>
+              <span className="breadcrumb-item active" aria-current="page">tu saldo es 20000</span>
+              </button>
+              </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
