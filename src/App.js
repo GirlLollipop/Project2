2154e4fa84d240152eed272be0dcfe56e3474470
DianaColm <dcolmenerog@yahoo.com.mx>
2018-04-21 10:44:43 -0500

    agregando estilos

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import React, { Component } from 'react'; //Este import va en todas las vistas, hace referencia a un pedacito de react
 import './App.css'; //el .css sirve para dar estilos
 import { Route, Switch } from "react-router-dom"; //hay que importar Route y switch ya que los ocuparemos
-import Home from'./components/Home.js' // importo componente de Home
-import LogIn from './components/LogIn.js' // importo componente de LogIn
-import Splash from 'react-splash';
-
+import Home from'./components/Home.js'; // importo componente de Home
+import LogIn from './components/LogIn.js'; // importo componente de LogIn
+import Logo from './components/images/logo-reto-bana.jpg';
+import './components/style/Style.css';
 
 
 class App extends Component {
   render() {
+    let imgStyle = {
+        width: '200px',
+        height: 'auto',
+        // borderRadius: '90px',
+        display: 'block',
+        margin: '0 auto',
+        paddinga: '0',
+      }
     return (
-      <div className="App">
+      <div>
 
-      <Splash
-  text="Project Name"
-  src="https://source.unsplash.com/FxU8KV7psMY/1600x900"
-  style={{ height: '70vh' }}
-/>
+      <nav className="padd navbar-dark bg-primary">
+        <img style={imgStyle} src={Logo} />
+      </nav>
+
+
 
       <Switch> //entre los switch escribimos los componentes que van a hacer un "cambio de vista"
         <Route component={LogIn} path="/" exact />
         <Route component={Home} path="/Home" />
       </Switch>
+
+      <footer className="footer">
+      <div className="container">
+        <span className="text-muted">Preferencias Ayuda</span>
+      </div>
+    </footer>
       </div>
     );
   }
